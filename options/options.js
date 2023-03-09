41894cc306c12ff33485f6c9cb2ef22c0440b1aa
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2023-03-10 00:01:36 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "sprint36a-dev-68096",
-  url: "https://sprint36a-dev-68096.botics.co",
-  api: "https://sprint36a-dev-68096.botics.co/api/v1"
-}
+  "name": "sprint36a-dev-68096",
+  "url": "https://sprint36a-dev-68096.botics.co",
+  "api": "https://sprint36a-dev-68096.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
-    copy: "Routes available!"
+    "copy": "Routes available!"
   }
-}
+};
